d019299b856a0558af9d686cd51a3b12d3adfff1
hdstarY <haodongy86@gmail.com>
2024-12-08 22:34:21 +0800

    雏形

---
+++ gulimall-order/src/main/java/com/hdstartY/Main.java
@@ -0,0 +1,12 @@
+package com.hdstartY;
+
+/**
+ * @author Dou li
+ * @creation data  2024-12-08-22:27
+ * @Description
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
